ac8d7bca08f9d631094c9f7f92082584f80bea1a
Nitya Zijoo <zijoonitya180@gmail.com>
2026-05-28 11:13:51 +0530

    fixing api

---
+++ Netlify/functions/weather.js
@@ -0,0 +1,40 @@
+exports.handler = async function(event) {
+
+    const city =
+    event.queryStringParameters.city
+
+    const apiKey =
+    process.env.API_KEY
+
+    const url =
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+
+    try {
+
+        const response =
+        await fetch(url)
+
+        const data =
+        await response.json()
+
+        return {
+
+            statusCode: 200,
+
+            body: JSON.stringify(data)
+        }
+
+    }
+
+    catch(error){
+
+        return {
+
+            statusCode: 500,
+
+            body: JSON.stringify({
+                error: "Failed to fetch weather"
+            })
+        }
+    }
+}
