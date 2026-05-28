8a06dc71e23bd58b2a40072278fd3b504ff1b27d
Nitya Zijoo <zijoonitya180@gmail.com>
2026-05-28 10:51:08 +0530

    hiding api

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ const searchButton = document.getElementById("search")
 
 async function getWeather(city){
 
-    const apiKey =
-    "474b2c5a9914ae4c17699a67f4aedb6a"
-
+    const apiKey = config.API_KEY
     const url =
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
 
